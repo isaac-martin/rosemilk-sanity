b74b594d033c51878fb314a31f7ee2b89a7e7ac4
Isaac Martin <isaac@brex.com>
2020-07-26 18:08:55 -0400

    added previews

--- deskStructure.js
+++ deskStructure.js
@@ -1,4 +1,6 @@
 import S from '@sanity/desk-tool/structure-builder'
+import IframePreview from './src/components/preview'
+
 import {
   MdSettings,
   MdAttachMoney,
@@ -8,6 +10,10 @@ import {
   MdLink,
   MdShoppingBasket
 } from 'react-icons/md'
+
+const remoteURL = 'https://rosemilk-ceramics-8662181615.gtsb.io/'
+const localURL = 'http://localhost:8000'
+const previewURL = window.location.hostname === 'localhost' ? localURL : remoteURL
 
 const hiddenDocTypes = listItem =>
   ![
@@ -30,17 +36,65 @@ export default () =>
         .title('Home Page')
         .icon(MdDescription)
         .schemaType('homePage')
-        .child(S.documentTypeList('homePage').title('Home Page')),
+        .child(
+          S.documentTypeList('homePage')
+            .title('Home Page')
+            .child(documentId =>
+              S.document()
+                .documentId(documentId)
+                .schemaType('homePage')
+                .views([
+                  S.view.form().icon(MdDescription),
+                  S.view
+                    .component(IframePreview)
+                    .icon(MdSettings)
+                    .title('Web Preview')
+                    .options({ previewURL })
+                ])
+            )
+        ),
       S.listItem()
         .title('Pages')
         .icon(MdDescription)
         .schemaType('page')
-        .child(S.documentTypeList('page').title('Pages')),
+        .child(
+          S.documentTypeList('page')
+            .title('Pages')
+            .child(documentId =>
+              S.document()
+                .documentId(documentId)
+                .schemaType('page')
+                .views([
+                  S.view.form().icon(MdDescription),
+                  S.view
+                    .component(IframePreview)
+                    .icon(MdSettings)
+                    .title('Web Preview')
+                    .options({ previewURL })
+                ])
+            )
+        ),
       S.listItem()
         .title('Products')
         .icon(MdAttachMoney)
         .schemaType('product')
-        .child(S.documentTypeList('product').title('Products')),
+        .child(
+          S.documentTypeList('product')
+            .title('Products')
+            .child(documentId =>
+              S.document()
+                .documentId(documentId)
+                .schemaType('product')
+                .views([
+                  S.view.form().icon(MdDescription),
+                  S.view
+                    .component(IframePreview)
+                    .icon(MdSettings)
+                    .title('Web Preview')
+                    .options({ previewURL })
+                ])
+            )
+        ),
       S.listItem()
         .title('Variants')
         .icon(MdAttachMoney)
@@ -75,9 +129,13 @@ export default () =>
             .schemaType('siteSettings')
             .documentId('siteSettings')
         ),
-
       // This returns an array of all the document types
       // defined in schema.js. We filter out those that we have
       // defined the structure above
+
       ...S.documentTypeListItems().filter(hiddenDocTypes)
     ])
+
+// Simple example of web preview
+
+// export default S.defaults()
---
+++ src/components/preview.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const slugGenerator = {
+  product: 'products'
+}
+const assembleProjectUrl = ({ displayed, options: { previewURL } }) => {
+  const { _type } = displayed
+
+  console.log(displayed)
+  let slug, urlPath
+  if (_type === 'product') {
+    slug = displayed.shopifyData.slug.current
+    urlPath = 'products/'
+  }
+
+  if (_type === 'page') {
+    slug = displayed.slug.current
+    urlPath = ''
+  }
+  if (_type === 'homePage') {
+    slug = '/'
+    urlPath = ''
+  }
+
+  if (!slug || !previewURL) {
+    console.warn('Missing slug or previewURL', { slug, previewURL })
+    return ''
+  }
+
+  const url = `${previewURL}/${urlPath}${slug}`
+  console.log('url', url)
+  return url
+}
+
+const IframePreview = ({ options, document: { displayed } }) => {
+  if (!displayed) {
+    return <p>There is no document to preview</p>
+  }
+
+  const url = assembleProjectUrl({ displayed, options })
+
+  if (!url) {
+    return <p>Having problems constructing the web front-end URL.</p>
+  }
+
+  return <iframe style={{ width: `100%`, height: `100%` }} src={url} frameBorder={'0'} />
+}
+
+export default IframePreview
